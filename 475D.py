899851bfd865fa622566ca14766e440e80e38d8f
Francisco O. Ayra Cáceres <franciscoayracaceress@gmail.com>
2024-07-08 23:35:26 -0400

    Via con ventanas deslizantes

--- 475D.py
+++ 475D.py
@@ -27,40 +27,25 @@ def SparseTable(array):
         return math.gcd(st[l][log], st[r-2**log+1][log])
 
 
-    def get_gcd_count(fixed_gcd):
-        def BinSearch(num, begin, sign_factor):
-            l, r = begin, n-1
-            last_index_found = -1
-            m=0
-            while(l < r):
-                m=(l+r)//2
-                beg_to_m_gcd = get(begin, m)
-                if beg_to_m_gcd == num:
-                    last_index_found = m
-                    
-                if sign_factor == -1:
-                    if beg_to_m_gcd <= num:
-                        r = m-1
-                    else:
-                        l = m+1
-                elif sign_factor == 1:
-                    if beg_to_m_gcd >= num:
-                        l = m+1
-                    else:
-                        r = m-1
-            if l==r and get(begin, r)==num: last_index_found=r
-            return last_index_found
-
-        result = 0
-        for l in range(n):
-            forward_limit = BinSearch(fixed_gcd, l, 1)
-            if forward_limit == -1:
-                continue
-            backward_limit = BinSearch(fixed_gcd, l, -1)
-            result += forward_limit - backward_limit + 1
+    def gcd_count(fixed_gcd):
+        result=0
+        r = 0
+        for l in range(0, n):
+            if l>r: r = l
+            continuee = False
+            while get(l, r) <= fixed_gcd:
+                if r-l == 0:
+                    continuee == True
+                    break
+                r-=1
+            if continuee: continue
+            while(r<n):
+                curr_gcd = get(l, r)
+                if curr_gcd < fixed_gcd: break
+                if curr_gcd == fixed_gcd:result+=1
+                r+=1
         return result
-
-    return st, get, get_gcd_count
+    return gcd_count
         
 # st, get = SparseTable([1, 2, 3, 4, 5, 6])
 # print(st)
@@ -80,7 +65,7 @@ def main():
     for i in range(q):
         queries.append(int(input().strip()))
 
-    st, get, get_gdc_count = SparseTable(array)
+    get_gdc_count = SparseTable(array)
 
     for q in queries:
         print(get_gdc_count(q))
